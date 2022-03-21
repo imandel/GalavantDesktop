af9faad8302c7ed09c7583a8593c38f1ff520024
Ning <45711839+chty627@users.noreply.github.com>
2022-03-21 19:12:58 -0400

    finished subtitles

--- src/Video/Subtitle.svelte
+++ src/Video/Subtitle.svelte
@@ -10,8 +10,16 @@
   const cfg = getContext("config");
 </script>
 
-<Button  on:pointerup={()=>{fileinput.click();}}>
-  <i class="fa-solid fa-closed-captioning " style="color:{$cfg.iconColor}">
-    <input bind:this={fileinput} style="display:none" type="file" accept=".vtt" bind:files />
-  </i>
+<style>
+  .center {
+      text-align: center;
+      width: 100%;
+      height: 100%;
+      margin-top: 20%;
+  }
+</style>
+
+<Button on:pointerup={()=>{fileinput.click();}}>
+  <input bind:this={fileinput} style="display:none" type="file" accept=".vtt" bind:files />
+  <i class="fa-solid fa-closed-captioning center" style="color:{$cfg.iconColor}"/>
 </Button>
